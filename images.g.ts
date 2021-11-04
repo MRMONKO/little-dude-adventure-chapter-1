b68339fd123b58dc6bdcf445cbf8a62f6e65e52c
MRMONKO <91742028+MRMONKO@users.noreply.github.com>
2021-11-04 15:03:39 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "myImage0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . 9 . . . . . . . 
+. . . . 9 1 1 1 1 1 1 . . . . . 
+. . . . 1 1 1 9 1 1 1 9 . . 9 . 
+. . . . e e e e e e e e 9 9 1 1 
+. . . . e e e e e e e e c c c c 
+. . . . e e f e e f e e c c c c 
+. . . . e e f e e f e e c c c c 
+. . . . e e e e e e e f c c c c 
+. . . . e f f e e e e e . b . . 
+. . . . e e f f f f f e . b . . 
+. . . . e e e e e e e . . b . . 
+. . . . . . . d . . . . . b . . 
+. . . . . . . d d d d d b b . . 
+. . . . . . . d . . . . . . . . 
+. . . . . . . d . . . . . . . . 
+. . . . . . d d d . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
